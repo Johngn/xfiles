c5ba428e30d3aa4ce3977f6b705da191c318e57a
John Gillan <jgillan@protonmail.com>
2020-07-29 20:59:50 +0200

    first plots created

---
+++ xfiles_eda.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+
+xfiles_episodes <- read_csv('./xfiles/xfiles_episodes_full.csv')
+
+season1 <- subset(xfiles_episodes, season == 1 | season == 2)
+season2 <- subset(xfiles_episodes, season == 2)
+  
+ggplot(season1) +
+  geom_point(aes(x = airdate,
+                 y = rating,
+                 size = votes,
+                 color = phenomena)) +
+  labs(y = "",
+       title = "IMDB ratings of season 1 of the X-files")
+
+ggplot(xfiles_episodes) +
+  geom_point(aes(x = number,
+                 y = rating,
+                 size = votes,
+                 color = phenomena)) +
+  labs(x = "",
+       y = "",
+       title = "IMDB ratings for X-files per season") +
+  facet_wrap(~ season)
+
+xfiles_episodes %>%
+  count(phenomena, sort = TRUE) %>%
+  mutate(phenomena = fct_reorder(phenomena, n)) %>%
+  ggplot(aes(n, phenomena)) +
+  labs(x = "",
+       y = "",
+       title = "Frequency of each type of phenomena in X-files episodes") +
+  geom_col()
